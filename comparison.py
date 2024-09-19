05219918f077ad4efc426a58e0732d8607cedaeb
franky2204 <fpantini1997@gmail.com>
2024-09-19 17:13:39 +0200

    fix

--- comparison.py
+++ comparison.py
@@ -60,12 +60,11 @@ def createMetaObjects(file_path):
     categorization_list = []
     with open(file_path, 'r') as infile:
         reader = csv.reader(infile, delimiter='\t')
-
         next(reader)
         sample_names = extract_sample(infile,1)
         sample_names = [name.replace("_output","") for name in sample_names]       
         unknown = extract_sample(infile,1)
-        unknown = [value for value in unknown]
+        unknown = [float(value) for value in unknown]
         
         for i in range(len(sample_names)):
             file_to_open =path_to_meta+sample_names[i]+"_output.txt"
@@ -80,7 +79,7 @@ def createMetaObjects(file_path):
                         categorization = Categorization("MetaPhlAn", sample_names[i], from_letter_to_taxa(letter),re.sub(r'[a-zA-Z]__', '', line_s[0]),name ,findClade(line_s[1]) ,percentual, ((percentual/(100-unknown[i]))*100)) 
                         categorization_list.append(categorization)
             # Write categorization_list to a file
-    return categorization_list  
+    return sample_names,categorization_list  
 
 def createKrakenObjects(file_path):
     categorization_list = []
@@ -91,7 +90,7 @@ def createKrakenObjects(file_path):
         sample_names = extract_sample(infile,8)
         infile.seek(0)
         type_names = extract_sample(infile,0)
-        print(sample_names)
+       # print(sample_names)
 
         for line_s in reader:
             percentual = line_s[8:]
@@ -117,21 +116,69 @@ def createKrakenObjects(file_path):
                                                  percentual[n],
                                                  percentual[n] ) 
                     categorization_list.append(categorization)
-    return categorization_list
+    return sample_names,categorization_list
 
+def compareFiles(metaObj,krakenObj):
+        comparison_results = []
+        meta_dict = {(obj.sample, obj.clade): obj for obj in metaObj}
+        kraken_dict = {(obj.sample, obj.clade): obj for obj in krakenObj}
 
+        all_keys = set(meta_dict.keys()).union(set(kraken_dict.keys()))
+
+        for key in all_keys:
+            meta_item = meta_dict.get(key)
+            kraken_item = kraken_dict.get(key)
+
+            if meta_item and kraken_item:
+                comparison_results.append({
+                    "sample": key[0],
+                    "clade": key[1],
+                    "meta_quantity": meta_item.quantity,
+                    "kraken_quantity": kraken_item.quantity,
+                    "difference": abs(meta_item.quantity - float(kraken_item.quantity))
+                })
+            elif meta_item:
+                comparison_results.append({
+                    "sample": key[0],
+                    "clade": key[1],
+                    "meta_quantity": meta_item.quantity,
+                    "kraken_quantity": 0,
+                    "difference": meta_item.quantity
+                })
+            elif kraken_item:
+                comparison_results.append({
+                    "sample": key[0],
+                    "clade": key[1],
+                    "meta_quantity": 0,
+                    "kraken_quantity": kraken_item.quantity,
+                    "difference": kraken_item.quantity
+                })
+
+        with open("comparison_results.txt", "w") as outfile:
+            for result in comparison_results:
+                outfile.write(f"{result['sample']}\t{result['clade']}\t{result['meta_quantity']}\t{result['kraken_quantity']}\t{result['difference']}\n")
+     
+def differenza_liste(lista1, lista2):
+    # Restituisce i nomi presenti in lista1 ma non in lista2
+    return [nome for nome in lista1 if nome not in lista2]
 
 
 krakenFile="kraken_taxa.tsv"
-#metaFile = "kraken_taxa.tsv"
-#metaObj=createMetaObjects(metaFile)
-krakenObj=createKrakenObjects(krakenFile)
+metaFile = "meta_table.tsv"
+mList,metaObj=createMetaObjects(metaFile)
+kList,krakenObj=createKrakenObjects(krakenFile)
 
-#with open("categorization_list.txt", "w") as outfile:
-#                for categorization in metaObj:
-#                    outfile.write(f"{categorization.tool}\t{categorization.sample}\t{categorization.depth}\t{categorization.three}\t{categorization.name}\t{categorization.clade}\t{categorization.quantity}\t{categorization.qtyWOU}\n")
+with open("categorization_list.txt", "w") as outfile:
+                for categorization in metaObj:
+                    outfile.write(f"{categorization.tool}\t{categorization.sample}\t{categorization.depth}\t{categorization.three}\t{categorization.name}\t{categorization.clade}\t{categorization.quantity}\t{categorization.qtyWOU}\n")
 
 
 with open("kraken_list.txt", "w") as outfile:
                 for categorization in krakenObj:
                     outfile.write(f"{categorization.tool}\t{categorization.sample}\t{categorization.depth}\t{categorization.three}\t{categorization.name}\t{categorization.clade}\t{categorization.quantity}\t{categorization.qtyWOU}\n")
+
+compareFiles(metaObj,krakenObj)
+#compareTotal(metaObj,krakenObj)
+
+
+print(differenza_liste(mList,kList))
